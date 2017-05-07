78a28dbe77f5276c778724d94e81977645d217f4
Clara <claraameller7@gmail.com>
2017-05-07 20:05:11 +0200

    Espero que l'últim commit de los tiempos

--- src/main/java/model/pojo/BeanTask.java
+++ src/main/java/model/pojo/BeanTask.java
@@ -20,50 +20,97 @@ public class BeanTask  implements Serializable {
     private Date completedAt;
     private int user_id;
 
+    /**
+     * Constructor per defecte d'una BeanTask
+     */
     public BeanTask() {
     
     }
 
+    /**
+     * Getter de l'Id
+     * @return id de la tasca
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Setter de l'id
+     * @param id nou a insertar
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Getter del text de la tasca
+     * @return el text de la tasca
+     */
     public String getTask() {
         return task;
     }
 
+    /**
+     * Setter del text de la tasca
+     * @param task el text de la tasca
+     */
     public void setTask(String task) {
         this.task = task;
     }
 
+    /**
+     * Getter de l'inici de la tasca
+     * @return la data inici
+     */
     public Date getStartedAt() {
         return startedAt;
     }
 
+    /**
+     * Setter de l'inici de la tasca
+     * @param startedAt la data inici
+     */
     public void setStartedAt(Date startedAt) {
         this.startedAt = startedAt;
     }
 
+    /**
+     * Getter del final de la tasca
+     * @return la data final
+     */
     public Date getCompletedAt() {
         return completedAt;
     }
-
+    
+    /**
+     * Setter del final de la tasca
+     * @param completedAt la data final
+     */
     public void setCompletedAt(Date completedAt) {
         this.completedAt = completedAt;
     }
 
+    /**
+     * Getter del id de l'usuari
+     * @return l'id de l'usuari
+     */
     public int getUser_id() {
         return user_id;
     }
 
+    /**
+     * Setter del id de l'usuari
+     * @param user_id l'id de l'usuari
+     */
     public void setUser_id(int user_id) {
         this.user_id = user_id;
     }
     
+    /**
+     * El toString()
+     * @return un String amb tots els atributs d'una tasca
+     */
     @Override
     public String toString() {
         return "BeanTask{" + "id=" + id + ", task=" + task + ", startedAt=" + startedAt + ", completedAt=" + completedAt + '}';
--- src/main/java/model/pojo/BeanUser.java
+++ src/main/java/model/pojo/BeanUser.java
@@ -18,43 +18,83 @@ public class BeanUser implements Serializable {
     private String email;
     private String password;
     
+    /**
+     * Constructor per defecte d'una BeanTask
+     */
     public BeanUser() {
         
     }
     
+    /**
+     * Constructor d'un BeanUser amb l'id i el nom d'usuari
+     * @param id
+     * @param username 
+     */
     public BeanUser(int id, String username){
         this.id = id;
         this.username = username;
     }
     
+    /**
+     * Getter de l'Id
+     * @return id de l'usuari
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Getter del nom d'usuari
+     * @return el nom d'usuari
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Setter del nom d'usuari
+     * @param username el nom d'usuari
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Getter de l'email
+     * @return l'email de l'usuari
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Setter de l'email
+     * @param email l'email de l'usuari
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Getter de la contrasenya
+     * @return la contrasenya de l'usuari
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Setter de la contrasenya
+     * @param password la contrasenya de l'usuari
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * El toString()
+     * @return un String amb tots els atributs d'un usuari
+     */
     @Override
     public String toString() {
         return "BeanUser{" + "id=" + id + ", username=" + username + ", email=" + email + ", password=" + password + '}';
